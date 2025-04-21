cc7a10a937aa30dfd0acc64d1f55dfc4f8ff5770
theo <thjmaurino@gmail.com>
2025-04-21 13:10:43 -0500

    finishing touches

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Copyright = () => {
   return (
-    <div className='w-full flex justify-center gap-3  mt-10 mb-10'>
-        <p className='text-gray-400 text-sm'>Silly useless site built by TM</p>
-        <p className='text-gray-400 text-sm'>♥</p>
+    <div className='w-full flex justify-center gap-3  mt-16 mb-5'>
+        <p className='text-gray-400 text-sm'>TM</p>
+        <p className='text-gray-400 px-5 text-sm'>♥</p>
         <p className='text-gray-400 text-sm'>2025 (c)</p>
 
 
